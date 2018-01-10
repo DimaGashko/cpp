3d0f88946389fb739255e983d45413693c7b0df4
DimaGashko <dgashko@mail.ua>
2018-01-10 22:09:16 +0200

    4730

---
+++ e-olimp/4730.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+long long fib(int n) {
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+
+	int l = 0, ll = 1;
+
+	for (int i = 2; i < n; i++) {
+		int prevLL = ll;
+
+		ll += l;
+		l = prevLL;
+	}
+
+	return l + ll;
+}
+
+int main() {
+	ifstream fin("input.txt");
+	ofstream fout("output.txt");
+
+	int n;
+	fin >> n;
+
+	long long res;
+
+	fout << fib(n + 1);
+
+	fin.close();
+	fout.close();
+	return 0;
+}
