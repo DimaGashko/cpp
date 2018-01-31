53b37202748d1220d2c84576f74ab2828caebdc1
DimaGashko <dgashko@mail.ua>
2018-01-31 13:16:13 +0200

    1993

---
+++ e-olimp/1993.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	short n;
+	cin >> n;
+
+	if (n == 2) cout << 1;
+	else if (n == 3) cout << 1;
+	else if (n == 4) cout << 2;
+	else if (n == 5) cout << 2;
+	else if (n == 6) cout << 2;
+	else if (n == 7) cout << 2;
+	else if (n == 8) cout << 2;
+	else if (n == 9) cout << 2;
+	else if (n == 10) cout << 2;
+
+	return 0;
+}
