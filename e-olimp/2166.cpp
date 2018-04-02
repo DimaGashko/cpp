677af6f2062c01a7f35f55997fc53c832d5398bd
DimaGashko <dgashko@mail.ua>
2018-04-02 17:43:48 +0300

    2166

---
+++ e-olimp/2166.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+int main() {
+	string s1, s2;
+	fin >> s1 >> s2;
+
+	sort(s1.begin(), s1.end());
+	sort(s2.begin(), s2.end());
+		
+	fout << (s1 == s2 ? "YES" : "NO") << endl;
+
+	fin.close();
+	fout.close();
+	return 0;
+}
