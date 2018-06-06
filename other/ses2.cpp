902e7bc93db91f4153b803b31d043ace50059701
DimaGashko <dgashko@mail.ua>
2018-06-06 21:25:24 +0300

    edit

--- other/ses2.cpp
+++ other/ses2.cpp
@@ -8,25 +8,58 @@ using namespace std;
 ifstream fin("input.txt");
 ofstream fout("output.txt");
 
+struct test {
+	test(int n) {
+		a = n;
+	}
+
+	int a;
+};
+
 void printArr(int *arr, int len);
 void writeArr(int *arr, int len);
+
+void printArr(int **arr, int m, int n);
+void writeArr(int **arr, int m, int n);
 
 void bubleSort(int *arr, int len);
 void selectSort(int *arr, int len);
 void insertionSort(int *arr, int len);
 
+void aa(istream &in, ostream &out) {
+	
+};
+
 int main() {
+	aa(fin, fout);
+	aa(cin, cout);
+
+	union {
+		int q, w;
+	};
+
+	cin >> q;
+	cout << q;
+
+	test t(999);
+	fout << t.a;
+
+	return 0;
+
 	srand(time(0));
 
 	int len = 10;
-	int *arr = new int[len];
+	int **arr = new int*[len];
+	for (int i = 0; i < len; i++) {
+		arr[i] = new int[len];
+	}
 
-	writeArr(arr, len);
-	printArr(arr, len);
+	writeArr(arr, len, len);
+	printArr(arr, len, len);
 
-	insertionSort(arr, len);
-	printArr(arr, len);
-
+	for (int i = 0; i < len; i++) {
+		delete[] arr[i];
+	}
 	delete[] arr;
 
 	fin.close();
@@ -80,5 +113,25 @@ void printArr(int *arr, int len) {
 void writeArr(int *arr, int len) {
 	for (int i = 0; i < len; i++) {
 		arr[i] = 10 + (rand() % 89);
+	}
+}
+
+void printArr(int **arr, int m, int n) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			fout << arr[i][j] << " ";
+		}
+
+		fout << endl;
+	}
+
+	fout << endl;
+}
+
+void writeArr(int **arr, int m, int n) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			arr[i][j] = 10 + (rand() % 89);
+		}
 	}
 }
