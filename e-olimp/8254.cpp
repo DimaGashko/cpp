eddac7e212a6ef11e9e9615aedbf9bfcfb7b1fde
DimaGashko <dgashko@mail.ua>
2019-03-23 18:54:14 +0200

    8254

---
+++ e-olimp/8254.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	long long n, m;
+	cin >> n >> m;
+
+	long long res = (n - 1) * m * 3;
+
+	cout << res << endl;
+
+	system("pause");
+	return 0;
+}
